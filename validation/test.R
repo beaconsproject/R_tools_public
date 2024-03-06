abbd8d5e0366eb0dee2873540d2e933ced9106ab
prvernier <pierre.vernier@gmail.com>
2024-03-05 16:20:23 -0800

    created script to test validation_functions

--- validation/test.R
+++ validation/test.R
@@ -1,14 +1,13 @@
 # Script to validate disturbance attributes
 
 # Load function
-source('validation/validate_functions.R')
+source('validation/validation_functions.R')
 
 # Function arguments
-
 ## data_pkg = name of data package
 ## lyr_line = name of linear disturbances layer
 ## lyr_poly = name of areal disturbances layer
 ## output_dir = location of output directory (will be created if it doesn't exist)
 
 # Examples using FDA_10AA_001
-validate_project(data_pkg='test_data/Data_package.gpkg', lyr_line='YG_Line', lyr_poly='YG_Poly', output_dir='output/test/')
+validate_project(data_pkg='test_data/Data_package.gpkg', lyr_line='YG_Line', lyr_poly='YG_Poly', output_dir='output/')
