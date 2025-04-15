fce86ca25cde4d306553d82450496e38629d5a2b
Melina Houle <houle.melina@gmail.com>
2025-04-15 14:20:36 -0400

    #2 - Automation of project_dependencies.R

--- createBuilderPolygons/project_dependencies.R
+++ createBuilderPolygons/project_dependencies.R
@@ -84,64 +84,6 @@ dissolve_catchments_from_table <- function(catchments_sf, input_table, out_featu
   }
   return(out_sf)
 }
-
-check_catchnum <- function(catchments_sf){
-  
-  # check CATCHNUM exists
-  if(!"CATCHNUM" %in% names(catchments_sf)){
-    stop("Catchments must contain column 'CATCHNUM'")
-  }
-}
-
-check_for_geometry <- function(in_sf){
-  
-  if(!"geometry" %in% names(in_sf)){
-    stop("Must contain column: geometry")
-  }
-}
-
-# Check for rows
-check_for_rows <- function(in_table){
-  if(nrow(in_table) == 0){
-    stop("input_table has no data")
-  }
-}
-
-# remove OID from tables comgin out of BUILDER
-remove_oid <- function(in_table){
-  if("OID" %in% colnames(in_table)){
-    out_table <- in_table %>%
-      dplyr::select(-OID)
-  } else{
-    out_table <- in_table
-  }
-  return(out_table)
-}
-
-# sep_network_names
-#' Split network names into their constituent parts.
-#'
-#' Takes a vector of network names in the form output by [gen_network_names()] (i.e. \code{"__"} separated),
-#' and splits the network names back into their original names.
-#' 
-#' If a single name is provided, a vector of names is returned. If a vector with length >1 is provided, a list
-#' of vectors is returned with each list element named with the network name.
-#'
-#' @param network_names String of a network name, or a vector of multiple network names. Must include the \code{"__"} separator.
-#'
-#' @return A vector of names, or a list of vectors.
-#' @export
-#'
-#' @examples
-#' sep_network_names("PB_1__PB_2")
-#' sep_network_names(c("PB_1__PB_2", "PB_1__PB_3"))
-#'
-#' x <- gen_network_names(c("PB_1", "PB_2", "PB_3", "PB_11"), 2)
-#' sep_network_names(x)
-#'
-#' x <- gen_network_names(c("PB_1", "PB_2", "PB_3", "PB_11"), 4)
-#' sep_network_names(x)
-
 sep_network_names <- function(network_names){
   
   if(length(network_names) == 1){
@@ -156,28 +98,19 @@ sep_network_names <- function(network_names){
   }
   return(out_val)
 }
-
-
-### get_catch_list ###
-#
-#' Get unique sets of catchment values from a table.
-#' 
-#' Designed for use with wide format tables such as those output by \code{get_upstream_catchments} where each 
-#' column name is a unique protected area id, and each row is a catchment id. Catchment values from all columns 
-#' included in pa_ids are combined and returned after removing duplicates.
-#' 
-#' @param pa_ids A single column name in input_table, or a vector of multiple column names.
-#' @param input_table Data frame where column names are protected area names and rows are catchments making 
-#' up each polygon. e.g. output from [get_upstream_catchments()].
-#'   
-#' @return A vector of unique catchment values.
-#' @importFrom magrittr %>%
-#' @importFrom rlang .data
-#' @export
-#'
-#' @examples
-#' get_catch_list("PB_0001", builder_table_sample)
-#' get_catch_list(c("PB_0001", "PB_0003"), builder_table_sample)
+check_catchnum <- function(catchments_sf){
+  
+  # check CATCHNUM exists
+  if(!"CATCHNUM" %in% names(catchments_sf)){
+    stop("Catchments must contain column 'CATCHNUM'")
+  }
+}
+check_catchnum_class <- function(catchments_sf, builder_table){
+  col_classes <- sapply(colnames(builder_table), function(x) class(builder_table[[x]]))
+  if(!all(col_classes == class(catchments_sf$CATCHNUM))){
+    warning(paste0("Table column classes do not match class(catchments_sf$CATCHNUM) for columns: ", paste0(colnames(builder_table)[col_classes != class(catchments_sf$CATCHNUM)], collapse=", ")))
+  }
+}
 get_catch_list <- function(pa_ids, input_table){
   
   # check nets are in input_table
@@ -191,4 +124,24 @@ get_catch_list <- function(pa_ids, input_table){
   })))
   
   net_catchments[!is.na(net_catchments)]
+}
+check_for_geometry <- function(in_sf){
+  
+  if(!"geometry" %in% names(in_sf)){
+    stop("Object must contain column: geometry")
+  }
+}
+remove_oid <- function(in_table){
+  if("OID" %in% colnames(in_table)){
+    out_table <- in_table %>%
+      dplyr::select(-OID)
+  } else{
+    out_table <- in_table
+  }
+  return(out_table)
+}
+check_for_rows <- function(in_table){
+  if(nrow(in_table) == 0){
+    stop("input_table has no data")
+  }
 }
